537359ba944c548f04f52f0994fc82a21ce1a11d
Anshuman202 <asanshuman2@gmail.com>
2022-12-24 12:29:50 +0530

    Username.cs

---
+++ Username.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+   public class Username
+   {
+        public static void DisplayUsername()
+        {
+
+            Console.WriteLine("please enter the username");
+
+            string username = Console.ReadLine();
+        }
+    
+    
+    
+   }
+}
